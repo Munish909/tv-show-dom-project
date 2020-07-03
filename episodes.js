e73fd0628e653f2549f61fe93dc7f7d715ba9e2c
Munish Jadhav <monish_jad@yahoo.com>
2020-07-03 15:08:21 +0100

    TVShowDomProjecLlevel100

--- episodes.js
+++ episodes.js
@@ -51,7 +51,7 @@ function getAllEpisodes() {
           "http://static.tvmaze.com/uploads/images/original_untouched/1/2668.jpg",
       },
       summary:
-        "<p>Lord Eddard Stark, ruler of the North, is summoned to court by his old friend, King Robert Baratheon, to serve as the King's Hand. Eddard reluctantly agrees after learning of a possible threat to the King's life. Eddard's bastard son Jon Snow must make a painful decision about his own future, while in the distant east Viserys Targaryen plots to reclaim his father's throne, usurped by Robert, by selling his sister in marriage.</p>",
+        "<p>Lord <i>Eddard Stark</i>, ruler of the North, is summoned to court by his old friend, King Robert Baratheon, to serve as the King's Hand. Eddard reluctantly agrees after learning of a possible threat to the King's life. Eddard's bastard son Jon Snow must make a painful decision about his own future, while in the distant east Viserys Targaryen plots to reclaim his father's throne, usurped by Robert, by selling his sister in marriage.</p>",
       _links: {
         self: {
           href: "http://api.tvmaze.com/episodes/4952",
--- script.js
+++ script.js
@@ -6,7 +6,64 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.className = "grid";
+  //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  
+  for (i = 0; i < episodeList.length; i++) {
+    console.log(episodeList[i].name, episodeList[i].image.medium, episodeList[i].summary);
+    console.log(episodeList[i].season, episodeList[i].number);
+
+   let card = document.createElement("article");
+   card.className = "myCard";
+   rootElem.appendChild(card); 
+
+   let title = document.createElement("h3");
+   title.innerText = episodeList[i].name;
+   card.appendChild(title);
+  
+   let episode = formatEpisodeCode(episodeList[i].season, episodeList[i].number);
+   //let episode = `E${episodeList[i].season}S${episodeList[i].number}`;
+
+   let episodeNumber = document.createElement("h3");
+   episodeNumber.innerText = episode;
+   card.appendChild(episodeNumber);
+
+   
+  
+
+   let image = document.createElement("img");
+   image.src = episodeList[i].image.medium;
+   card.appendChild(image);
+
+   let para = document.createElement("p");
+   para.innerHTML = episodeList[i].summary;
+   para.className = "mySummary";
+   card.appendChild(para);
+
+   let apiLink = document.createElement("a");
+   apiLink.innerText = episodeList[i]._links.self.href;
+   card.appendChild(apiLink);
+  
+   apiLink.href = episodeList[i]._links.self.href;
+   
+  
+  }
+}
+
+function formatEpisodeCode(seasonNumber, episodeNumber){
+  //let seasonPadding = "";
+  //let episodePadding = "";
+  if(seasonNumber < 10){
+   //seasonPadding = "0";
+   seasonNumber = "0".concat(seasonNumber);
+}
+if (episodeNumber < 10){
+  //episodePadding = "0";
+  episodeNumber = "0".concat(episodeNumber);
+}
+//return "S" + seasonPadding + seasonNumber + "E" + episodePadding + episodeNumber;
+return "S" + seasonNumber + "E" + episodeNumber;
 }
 
 window.onload = setup;
+
